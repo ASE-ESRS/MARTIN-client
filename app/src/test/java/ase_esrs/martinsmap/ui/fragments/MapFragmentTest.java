7c1a839575679574ee8cac2225840a1ef0973ebb
Dan <danny_arthur@hotmail.co.uk>
2017-11-23 13:21:35 +0000

    Added Unit Test Skeleton

---
+++ app/src/test/java/ase_esrs/martinsmap/ui/fragments/MapFragmentTest.java
@@ -0,0 +1,68 @@
+package ase_esrs.martinsmap.ui.fragments;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by danny on 23/11/2017.
+ */
+public class MapFragmentTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void onResume() throws Exception {
+
+    }
+
+    @Test
+    public void onPause() throws Exception {
+
+    }
+
+    @Test
+    public void onMapReady() throws Exception {
+
+    }
+
+    @Test
+    public void buildGoogleApiClient() throws Exception {
+
+    }
+
+    @Test
+    public void onConnected() throws Exception {
+
+    }
+
+    @Test
+    public void onConnectionSuspended() throws Exception {
+
+    }
+
+    @Test
+    public void onConnectionFailed() throws Exception {
+
+    }
+
+    @Test
+    public void onLocationChanged() throws Exception {
+
+    }
+
+    @Test
+    public void onRequestPermissionsResult() throws Exception {
+
+    }
+
+}
