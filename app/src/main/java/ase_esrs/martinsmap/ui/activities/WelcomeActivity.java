2e0247374c53da22a7740708410a93c1b1fb8e94
Dan <danny_arthur@hotmail.co.uk>
2017-11-23 21:35:05 +0000

    Code Coverage Tool Fix again

---
+++ app/src/main/java/ase_esrs/martinsmap/ui/activities/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package ase_esrs.martinsmap.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ase_esrs.martinsmap.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
