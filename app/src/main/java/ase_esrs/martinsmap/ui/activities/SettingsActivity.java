c01e6075c127f8015379ad67fd5cde9e89bee5a9
Loic Verrall <loic.verrall@gmail.com>
2017-11-23 18:26:43 +0000

    Settings screen filled in a bit

--- app/src/main/java/ase_esrs/martinsmap/ui/activities/SettingsActivity.java
+++ app/src/main/java/ase_esrs/martinsmap/ui/activities/SettingsActivity.java
@@ -7,8 +7,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 
 import ase_esrs.martinsmap.R;
 
@@ -28,6 +31,31 @@ public class SettingsActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
-    }
 
+        // Add Save Radius button handler.
+        Button clickButton = (Button) findViewById(R.id.saveRadiusButton);
+        clickButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Update Search Radius button pressed.
+            }
+        });
+
+        // Add the value view to the seek bar to show the current radius.
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                int val = (progress * (seekBar.getWidth() - 2 * seekBar.getThumbOffset())) / seekBar.getMax();
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) { }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) { }
+        });
+    }
 }
