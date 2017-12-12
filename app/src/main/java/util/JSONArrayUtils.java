4626c601cd38a5d1eb5a936cd21550eb3e699b50
Mark Paice <msp26@sussex.ac.uk>
2017-12-12 12:27:51 +0000

    Added Javadoc, removed radius/distance request to AWS Server.

--- app/src/main/java/util/JSONArrayUtils.java
+++ app/src/main/java/util/JSONArrayUtils.java
@@ -3,8 +3,19 @@ package util;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+/**
+ * Utility methods for house price data within JSONArrays
+ * @author Dan and Mark
+ */
 public interface JSONArrayUtils {
 
+    /**
+     * Returns the smallest house price within the JSONArray
+     * @param array The JSONArray of house price data
+     * @return The smallest house price
+     * @throws JSONException
+     * @author Mark
+     */
     public static int getMinPrice(JSONArray array) throws JSONException {
         int min = array.getJSONObject(0).getInt("price");
         for (int i = 1; i < array.length(); i++) {
@@ -14,6 +25,13 @@ public interface JSONArrayUtils {
         return min;
     }
 
+    /**
+     * Returns the largest house price within the JSONArray
+     * @param array The JSONArray of house price data
+     * @return The largest house price
+     * @throws JSONException
+     * @author Mark
+     */
     public static int getMaxPrice(JSONArray array) throws JSONException {
         int max = array.getJSONObject(0).getInt("price");
         for (int i = 1; i < array.length(); i++) {
@@ -23,6 +41,13 @@ public interface JSONArrayUtils {
         return max;
     }
 
+    /**
+     * Returns the average of the house prices within the JSONArray
+     * @param array The JSONArray of house price data
+     * @return The average house price
+     * @throws JSONException
+     * @author Mark
+     */
     public static int getAveragePrice(JSONArray array) throws JSONException {
         long total = 0;
         for (int i = 0; i < array.length(); i++) {
